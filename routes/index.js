9617f24b5576ba08d63911f837bf42c6f21b76c5
Josselin Bonnaud <josselin.bonnaud@gmail.com>
2022-09-28 12:37:01 +0200

    change apikey

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var fetch = require('node-fetch');
 const api_key = process.env.API_KEY;
 
 router.get('/movies', (req, res) => {
-    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=0e782c931f8adb40a66dab66f8668126`)
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`)
     .then(res => res.json())
     .then(data => {
         res.json({ movies: data.results })
